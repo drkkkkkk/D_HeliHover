696d76a110c820ad4f6a4c04f68b1af06f8cb965
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-04 18:39:33 +0000

    Main File
    Enjoy hovering! You can hover by clicking K or what ever you set in the config. Dont crash i gues....

---
+++ Drk_Helihover/config/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.HoverKeyBind = "F1" -- You can change this to whatever keybind you'd like. ( F1, F2, F3, F4 ETC Do NOT Work.)
+Config.HoverChatMessage = "You are now hovering!" -- What ever you want the text for the chat message to say when you start Hovering.
+Config.UnHoverChatMessage = "You have stopped hovering!"
+
+Config.HelicopterCantHoverMsg = "The helicopter you are in is not one that can hover!"
+
+Config.NoHelicopterMessage = "You're not in a helicopter!"
+
+Config.Helicopters = {"polmav", "frogger"}
